98cbb068edad142edf0185954bafd57d8df81a2f
Kazanchev Nikita <KazanchevNikita89@yandex.ru>
2023-03-07 00:21:15 +0300

    the task about taxes

--- company.py
+++ company.py
@@ -15,6 +15,18 @@
 10. Вывести названия должностей, которые получают больше 90к без повторений.
 11. Посчитать среднюю зарплату по каждому отделу среди девушек (их зовут Мишель, Николь, Кристина и Кейтлин).
 12. Вывести без повторений имена людей, чьи фамилии заканчиваются на гласную букву.
+
+Третий уровень:
+Теперь вам пригодится ещё список taxes, в котором хранится информация о налогах на сотрудников из разных департаметов.
+Если department None, значит, этот налог применяется ко всем сотрудникам компании.
+Иначе он применяется только к сотрудникам департмента, название которого совпадает с тем, что записано по ключу department.
+К одному сотруднику может применяться несколько налогов.
+13. Вывести список отделов с суммарным налогом на сотрудников этого отдела.
+14. Вывести список всех сотредников с указанием зарплаты "на руки" и зарплаты с учётом налогов.
+15. Вывести список отделов с указанием месячной налоговой нагрузки – количеством денег, которые в месяц этот отдел платит налогами.
+16. Вывести список отделов, отсортированный по месячной налоговой нагрузки.
+17. Вывести всех сотрудников, за которых компания платит больше 100к налогов в год.
+18. Вывести имя и фамилию сотрудника, за которого компания платит меньше всего налогов.
 """
 
 departments = [
@@ -38,99 +50,130 @@ departments = [
     },
 ]
 
-# 1. Вывести названия всех отделов
-for department in departments:
-    print(department["title"])
+tesax = [
+    {"department": None, "name": "vat", "value_percents": 13},
+    {"department": "IT department", "name": "hiring", "value_percents": 6},
+    {"department": "BizDev Department", "name": "sales", "value_percents": 20},
+]
+
+# # 1. Вывести названия всех отделов
+# for department in departments:
+#     print(department["title"])
     
 
-# 2. Вывести имена всех сотрудников компании.
-for department in departments:
-    for name in department["employers"]:
-        print(name["first_name"])
+# # 2. Вывести имена всех сотрудников компании.
+# for department in departments:
+#     for name in department["employers"]:
+#         print(name["first_name"])
 
-# 3. Вывести имена всех сотрудников компании с указанием отдела, в котором они работают.
-for department in departments:
-    for name in department["employers"]:
-        print(name["first_name"], department["title"])
+# # 3. Вывести имена всех сотрудников компании с указанием отдела, в котором они работают.
+# for department in departments:
+#     for name in department["employers"]:
+#         print(name["first_name"], department["title"])
     
 
 
-# 4. Вывести имена всех сотрудников компании, которые получают больше 100к.
-for department in departments:
-    for name in department["employers"]:
-        if name["salary_rub"] > 100000:
-            print(name["first_name"])
+# # 4. Вывести имена всех сотрудников компании, которые получают больше 100к.
+# for department in departments:
+#     for name in department["employers"]:
+#         if name["salary_rub"] > 100000:
+#             print(name["first_name"])
     
 
-# 5. Вывести позиции, на которых люди получают меньше 80к (можно с повторениями).
+# # 5. Вывести позиции, на которых люди получают меньше 80к (можно с повторениями).
+# for department in departments:
+#     for name in department["employers"]:
+#         if name["salary_rub"] < 80000:
+#             print(name["position"])
+
+
+# # 6. Посчитать, сколько денег в месяц уходит на каждый отдел – и вывести вместе с названием отдела
+# for department in departments:
+#     total_salary = 0
+#     for name in department["employers"]:
+#         total_salary += name["salary_rub"]
+#     print(department["title"], total_salary)
+
+
+# # 7. Вывести названия отделов с указанием минимальной зарплаты в нём.
+# for department in departments:
+#     salaries = []
+#     for name in department["employers"]:
+#         salaries.append(name["salary_rub"])
+#     print(f'Минимальной зарплаты {department["title"]} {min(salaries)}')
+
+
+# # 8. Вывести названия отделов с указанием минимальной, средней и максимальной зарплаты в нём.
+# for department in departments:
+#     salaries = []
+#     for name in department["employers"]:
+#         salaries.append(name["salary_rub"])
+#     min_wage = min(salaries)
+#     max_wage = max(salaries)
+#     average_salary = sum(salaries) / len(department["employers"])
+#     print(f'{department["title"]} минимальная {min_wage}, среднаяя {average_salary}, максимальная {max_wage}, зарплаты.')
+
+
+# # 9. Вывести среднюю зарплату по всей компании.
+# sum_money = 0
+# total_employees = 0
+# for department in departments:
+#     total_employees += len(department["employers"])
+#     for name in department["employers"]:
+#         sum_money += name["salary_rub"]
+# print(f'{sum_money / total_employees} средняя зарплата по всей компании')
+
+
+# # 10. Вывести названия должностей, которые получают больше 90к без повторений.
+# positions = []
+# for department in departments:
+#     for name in department["employers"]:
+#         if name["salary_rub"] > 90000:
+#             positions.append(name["position"])
+# print(' '.join(set(positions)))
+
+
+# # 11. Посчитать среднюю зарплату по каждому отделу среди девушек (их зовут Мишель, Николь, Кристина и Кейтлин).
+# girls = {"Michelle", "Nicole", "Christina", "Caitlin"}
+# for department in departments:
+#     salary_girls = []
+#     for name in department["employers"]:
+#         if name["first_name"] in girls:
+#             salary_girls.append(name["salary_rub"])
+#     average_salary_girls = round(sum(salary_girls) / len(salary_girls), 2)
+#     print(f'Средняя зарплата девушек {department["title"]} {average_salary_girls}')
+
+
+# # 12. Вывести без повторений имена людей, чьи фамилии заканчиваются на гласную букву.
+# vowels = set("aeiouy")
+# employee_name = []
+# for department in departments:
+#     for name in department["employers"]:
+#         if name["last_name"][-1] in vowels:
+#             employee_name.append(name["first_name"])
+# print("Люди, чьи фамилии заканчиваются на гласную букву", ' '.join(set(employee_name)))
+
+
+# 13. Вывести список отделов с суммарным налогом на сотрудников этого отдела.
+department_None = {}
+department_matches = {}
 for department in departments:
-    for name in department["employers"]:
-        if name["salary_rub"] < 80000:
-            print(name["position"])
-
-
-# 6. Посчитать, сколько денег в месяц уходит на каждый отдел – и вывести вместе с названием отдела
-for department in departments:
-    total_salary = 0
-    for name in department["employers"]:
-        total_salary += name["salary_rub"]
-    print(department["title"], total_salary)
-
-
-# 7. Вывести названия отделов с указанием минимальной зарплаты в нём.
-for department in departments:
-    salaries = []
-    for name in department["employers"]:
-        salaries.append(name["salary_rub"])
-    print(f'Минимальной зарплаты {department["title"]} {min(salaries)}')
-
-
-# 8. Вывести названия отделов с указанием минимальной, средней и максимальной зарплаты в нём.
-for department in departments:
-    salaries = []
-    for name in department["employers"]:
-        salaries.append(name["salary_rub"])
-    min_wage = min(salaries)
-    max_wage = max(salaries)
-    average_salary = sum(salaries) / len(department["employers"])
-    print(f'{department["title"]} минимальная {min_wage}, среднаяя {average_salary}, максимальная {max_wage}, зарплаты.')
-
-
-# 9. Вывести среднюю зарплату по всей компании.
-sum_money = 0
-total_employees = 0
-for department in departments:
-    total_employees += len(department["employers"])
+    sum_money = 0
     for name in department["employers"]:
         sum_money += name["salary_rub"]
-print(f'{sum_money / total_employees} средняя зарплата по всей компании')
+            
+    for tax in tesax:
+        if department['title'] == tax['department']:
+            department_matches[department['title']] = sum_money  * (tesax[0]['value_percents'] + tesax[1]['value_percents']) / 100
+        else:
+            department_None[department['title']] = sum_money  * tesax[0]['value_percents'] / 100
+print(department_matches)
+print(department_None)
+                 
 
-
-# 10. Вывести названия должностей, которые получают больше 90к без повторений.
-positions = []
-for department in departments:
-    for name in department["employers"]:
-        if name["salary_rub"] > 90000:
-            positions.append(name["position"])
-print(' '.join(set(positions)))
-
-
-# 11. Посчитать среднюю зарплату по каждому отделу среди девушек (их зовут Мишель, Николь, Кристина и Кейтлин).
-girls = {"Michelle", "Nicole", "Christina", "Caitlin"}
-for department in departments:
-    salary_girls = []
-    for name in department["employers"]:
-        if name["first_name"] in girls:
-            salary_girls.append(name["salary_rub"])
-    average_salary_girls = round(sum(salary_girls) / len(salary_girls), 2)
-    print(f'Средняя зарплата девушек {department["title"]} {average_salary_girls}')
-
-
-# 12. Вывести без повторений имена людей, чьи фамилии заканчиваются на гласную букву.
-vowels = set("aeiouy")
-employee_name = []
-for department in departments:
-    for name in department["employers"]:
-        if name["last_name"][-1] in vowels:
-            employee_name.append(name["first_name"])
-print("Люди, чьи фамилии заканчиваются на гласную букву", ' '.join(set((employee_name))))
+        
+#Теперь вам пригодится ещё список taxes, в котором хранится информация о налогах на сотрудников из разных департаметов.
+#Если department None, значит, этот налог применяется ко всем сотрудникам компании.
+#Иначе он применяется только к сотрудникам департмента, название которого совпадает с тем, что записано по ключу department.
+#К одному сотруднику может применяться несколько налогов.
+#13. Вывести список отделов с суммарным налогом на сотрудников этого отдела.
